d3bff9d238277281af7f7c2f10cda2164e18ab78
Alexis Mata WebDev <alexedmata@gmail.com>
2022-10-26 11:50:49 -0400

    #03 User controller login() y documentacion

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,12 +7,17 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
 class UserController extends Controller
 {
+    /**
+     * Performs the registration of a new user.
+     *
+     * @param Request $request
+     * @return mixed
+     */
     public function register(Request $request)
     {
-       $validate = $this->validateData($request);
+       $validate = $this->validateData($request,'register');
 
        $user = User::create([
             'fullname' => $validate['fullname'],
@@ -20,38 +25,104 @@ class UserController extends Controller
             'username' => $validate['username'],
             'password' => Hash::make($validate['password'])
        ]);
-       
-       $user->save();
-
 
         return response()->json([
             "Status" => 1,
-            "Data" => $user->toArray(),
+            "Data" => $user->only('fullname','email','username'),
             "Msg" => "Registro Exitoso."
             ]);
 
     }
+    /**
+     * Performs the login of the user.
+     *
+     * @param Request $request
+     * @return mixed
+     */
     public function login(Request $request)
     {
-        //
+        $validate = $this->validateData($request,'login');
+
+        $user = User::where('username', $validate['username'])->first();
+        if (isset($user->id)   ){
+
+            $info = $this->checkPasswordLogin($user, $validate['password']);
+
+            return response()->json($info, $info['code_response']);
+        }else{
+            return response()->json([
+                "code_response" => 404,
+                "status" => 0,
+                "Msg" => "El usuario no se encuentra registrado."
+                ], 404);
+        }
     }
     public function profile()
     {
-        //
+       //
     }
     public function logout()
     {
         //
     }
-
-    public function validateData(Request $request)
+    /**
+     * Validates the data submitted in the type of request that sends it.
+     * the type can be register o login.
+     *
+     * @param Request $request
+     * @param string $type
+     * @return mixed
+     */
+    private function validateData(Request $request,string $type)
     {
-        return $request->validate([
-            'fullname' => 'required|string|max:50',
-            'email' =>  'required|string|email|unique:users',
-            'username' => 'required|string|min:8',
-            'password'=> 'required|confirmed'
-        ]);
+        if ($type === 'register'){
+            $rules =[
+                'fullname' => 'required|string|max:50',
+                'email' =>  'required|string|email|unique:users',
+                'username' => 'required|string|min:8',
+                'password'=> 'required|confirmed'
+            ];
+        }else{
+            $rules = [
+                'username' => 'required|string',
+                'password'=> 'required'
+            ];
+        }
+
+        return $request->validate($rules);
+    }
+   /**
+    * Check if the password typed and the password stored in the DB are the same.
+    *
+    * If both password are the same then create the token to user login,
+    * else return The password does not match our records.
+    *
+    * @param User $user
+    * @param string $inputPassword
+    * @return array
+    */
+    private function checkPasswordLogin(User $user, string $inputPassword)
+    {
+
+        if (Hash::check($inputPassword, $user->password)) {
+            $userToken = $user->createToken('auth_token')->plainTextToken;
+            return [
+                "code_response" => 200,
+                "status" => 1,
+                "token_access" => $userToken,
+                "token_type" => 'Bearer',
+                "Msg" => "Ha iniciado sesión exitosamente."
+
+                ];
+
+        }else{
+            return [
+                "code_response" => 401,
+                "status" => 0,
+                "Msg" => "La contraseña no coincide con nuestros registros."
+                ];
+        }
+
     }
      /**
      * Display a listing of the resource.
